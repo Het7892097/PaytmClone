b61277bfd19bf1543dcc4c17c0f77fc827055749
Het <96581546+Het7892097@users.noreply.github.com>
2024-09-23 20:25:15 +0530

    FinalChanges

--- Backend_API/Backend_API/obj/Debug/net8.0/Backend_API.AssemblyInfo.cs
+++ Backend_API/Backend_API/obj/Debug/net8.0/Backend_API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19ed5570c809b40b011a4ec82176081d0caca888")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
